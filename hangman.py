bd9ead72d869397e51c210ea1cea99d880771008
Shreyas Panale <shreya.panale@gmail.com>
2020-10-25 00:24:19 +0530

    Improved UI

--- hangman.py
+++ hangman.py
@@ -38,7 +38,12 @@ def hangman(tally):
                 b[i]=x
                 b="".join(b)
                 points=points+1
-        if(buffer==1 or found==0):
+        if(buffer==1):
+            print("Correct guess :)")
+            for j in range (0, len(b)):
+                print(b[j] ,end= ' ')
+        if(found==0):
+            print("Inorrect guess, try harder!!")
             for j in range (0, len(b)):
                 print(b[j] ,end= ' ')
         if(buffer==0):
